72b7f897be88c74e25f166042a12066d38746cb1
Chris Breaux and Tate Easterbrooks <chris@>
2019-01-14 16:28:56 -0800

    add two variables to find the first and last character in a user generated sentence

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var sentence = prompt("Please enter a sentence.");
+console.log(sentence);
+
+
+var firstLetter = sentence.charAt(0);
+var lastLetter = sentence.charAt(sentence.length - 1);
+
+
+alert(firstLetter.toUpperCase());
+alert(lastLetter.toUpperCase());
